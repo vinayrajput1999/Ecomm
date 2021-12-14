2eb7aa24f86d420ba130f9d1221f3d9332998e52
vinay <rajvinay834@gmail.com>
2021-12-14 12:47:37 +0530

    Ecomm Carts

---
+++ src/data.js
@@ -0,0 +1,39 @@
+import img1 from './img/img1.jpg';
+import img2 from './img/img2.jpg';
+import img3 from './img/img3.jpg';
+import img4 from './img/img4.jpg';
+
+const data = {
+    productData:[
+        {
+           id: 1,
+           img: img1,
+           title: 'Hand Bag',
+           desc: '',
+           price: 90,
+        },
+        {
+            id: 2,
+            img: img2,
+            title: 'Cams',
+            desc: '',
+            price: 100,
+         },
+         {
+            id: 3,
+            img: img3,
+            title: 'Apple Watch',
+            desc: '',
+            price: 50,
+         },
+         {
+            id: 4,
+            img: img4,
+            title: 'Laptop',
+            desc: '',
+            price: 500,
+         },
+    ],
+};
+
+export default data;
